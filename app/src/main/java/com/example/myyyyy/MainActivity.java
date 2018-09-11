2b0448a2a999855a0f38359233971f135a99401a
Priya <priyasethu9@gmail.com>
2018-09-11 11:49:26 +0530

    THIRD commit

--- app/src/main/java/com/example/myyyyy/MainActivity.java
+++ app/src/main/java/com/example/myyyyy/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(),"This is my first Android Studio with Github",Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),"This is my first Android Studio project  with Github",Toast.LENGTH_LONG).show();
     }
 }
